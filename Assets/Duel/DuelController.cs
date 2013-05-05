99ab944e51965e7bbbf1cf459b0c9abf718349d1
Patrick McKenna <patrick.a.mckenna@gmail.com>
2013-05-05 19:58:42 -0400

    add duel controller (broken) and update build settings

---
+++ Assets/Duel/DuelController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+
+public class DuelController : MonoBehaviour {
+	
+	String rootString;
+	
+	public int controller;
+	
+	void Start () {
+		rootString = GetComponentInChildren<String>();
+	}
+	
+	bool extending = false;
+	bool retracting = false;
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (!extending = Input.GetButtonDown("Extend1")) {
+			//rootString.
+		}
+		*/
+		
+		/*
+		float x = Input.GetAxis("Horizontal"+controller);
+		float y = Input.GetAxis("Vertical1"+controller);
+		Vector3 force = new Vector3(x, 0, y);
+		force.Normalize();
+		*/
+		rigidbody.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical"+controller));
+		rigidbody.AddRelativeTorque(Vector3.up * Input.GetAxis("Horizonta"+controller));
+		//rigidbody.AddRelativeForce
+		if (Input.GetButtonDown("Retract2")) {
+			Debug.Log("retract");
+		}
+		
+		Debug.Log("extend"+Input.GetAxis("Extend1"));
+		Debug.Log("re"+Input.GetAxis("Retract1"));
+	}
+}
